281dde38f7eaabe1a5203632ab50629f801fa54f
jampizan <20253188@s.ubaguio.edu>
2026-05-12 22:07:54 +0800

    Fix deployment path and white screen

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-plugins: [react()],
-base: '/Portfolio project/',
+  base: '/Portfolio-project/', 
+  plugins: [react()],
 })
